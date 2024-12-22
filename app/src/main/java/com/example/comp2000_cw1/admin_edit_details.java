6b8e98ad1ad7deff32863ea9ba3ba3716be65d18
Chris-Bratosin <chris.bratosin@students.plymouth.ac.uk>
2024-12-22 04:46:41 +0000

    converted fragments back to activites, re-inputted the code alongside some changes to it. fixed some errors in admin_login.java and other pages. Figured out some other issues

--- app/src/main/java/com/example/comp2000_cw1/admin_edit_details.java
+++ app/src/main/java/com/example/comp2000_cw1/admin_edit_details.java
@@ -2,63 +2,23 @@ package com.example.comp2000_cw1;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link admin_edit_details#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class admin_edit_details extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public admin_edit_details() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment admin_edit_details.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static admin_edit_details newInstance(String param1, String param2) {
-        admin_edit_details fragment = new admin_edit_details();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class admin_edit_details extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_edit_details, container, false);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_edit_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
