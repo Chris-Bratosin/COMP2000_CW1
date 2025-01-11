5f48298a367ea3fda67b5d7a395dc5f196cb6f87
Chris-Bratosin <chris.bratosin@students.plymouth.ac.uk>
2025-01-11 00:44:51 +0000

    finished re-design of notifications page

--- app/src/main/java/com/example/comp2000_cw1/employee_notifications.java
+++ app/src/main/java/com/example/comp2000_cw1/employee_notifications.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class forgot_password_page extends AppCompatActivity {
+public class employee_notifications extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_forgot_password_page);
+        setContentView(R.layout.employee_notifications);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/comp2000_cw1/sign_in_page.java
+++ app/src/main/java/com/example/comp2000_cw1/sign_in_page.java
@@ -8,17 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class employee_main_hub extends AppCompatActivity {
+public class sign_in_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_employee_main_hub);
+        setContentView(R.layout.sign_in_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+   
+
 }
+
+
