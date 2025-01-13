233cf038a06b7ec3f4bc73319ca90add7bb0228b
Chris-Bratosin <chris.bratosin@students.plymouth.ac.uk>
2025-01-13 00:20:48 +0000

    re-designed page for creating an employee

--- app/src/main/java/com/example/comp2000_cw1/employee_request_holiday.java
+++ app/src/main/java/com/example/comp2000_cw1/employee_request_holiday.java
@@ -14,7 +14,7 @@ public class employee_request_holiday extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_employee_request_holiday);
+        setContentView(R.layout.employee_request_holiday);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
